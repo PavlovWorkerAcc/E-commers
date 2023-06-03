82ded9d0fca233ebfaa4418cbf652ff867b0758e
Dima2323-tech <123169116+PavlovWorkerAcc@users.noreply.github.com>
2023-06-03 21:56:03 +0300

    Layout

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader
 const config = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
   },
   resolve: {
